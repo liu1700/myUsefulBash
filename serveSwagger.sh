f40e31b99a7a8f672926c5f53e4c06eb0729758f
Liu <liu170045@gmail.com>
2018-07-03 16:52:48 +0800

    Update serveSwagger.sh

--- serveSwagger.sh
+++ serveSwagger.sh
@@ -2,10 +2,12 @@
 # npm install -g http-server
 # docker pull swaggerapi/swagger-ui
 
-echo "swagger ui web will host at port http://127.0.0.1:7070"
-echo "access the static swagger file at port http://127.0.0.1:7071"
+echo "swagger ui web will host at http://127.0.0.1:7070"
+echo "access the static swagger file at http://127.0.0.1:7071/yourSwaggerFileName.json(or .yml)"
 
-SWAGGER_FOLDER_PATH = $1
+SWAGGER_FOLDER_PATH="$1"
+
+echo "docker container id:"
 docker run -d -p 7070:8080 swaggerapi/swagger-ui
 cd $SWAGGER_FOLDER_PATH
 http-server -p 7071 --cors

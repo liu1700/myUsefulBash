2c3e9a3106ec69d32f08daf243b4a89f8ca9ed01
Liu <liu170045@gmail.com>
2018-07-03 16:54:35 +0800

    Update serveSwagger.sh

--- serveSwagger.sh
+++ serveSwagger.sh
@@ -1,6 +1,7 @@
 # install dependency
 # npm install -g http-server
 # docker pull swaggerapi/swagger-ui
+# Usage: serveSwagger.sh /Path/to/your/swaggerFile/Folder
 
 echo "swagger ui web will host at http://127.0.0.1:7070"
 echo "access the static swagger file at http://127.0.0.1:7071/yourSwaggerFileName.json(or .yml)"

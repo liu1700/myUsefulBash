307d4ec682524ea7dfa2bc3d3a5785a14951df39
Liu <liu170045@gmail.com>
2018-07-03 16:42:40 +0800

    Update serveSwagger.sh

--- serveSwagger.sh
+++ serveSwagger.sh
@@ -6,6 +6,6 @@ echo "swagger ui web will host at port http://127.0.0.1:7070"
 echo "access the static swagger file at port http://127.0.0.1:7071"
 
 SWAGGER_FOLDER_PATH = $1
-docker run -p -d 7070:8080 swaggerapi/swagger-ui
+docker run -d -p 7070:8080 swaggerapi/swagger-ui
 cd $SWAGGER_FOLDER_PATH
 http-server -p 7071 --cors
